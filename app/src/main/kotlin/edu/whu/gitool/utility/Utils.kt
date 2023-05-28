385547c5dbc8efa8285168737dddd89a27b6bbc5
Hwa <hwahe.cs@gmail.com>
2023-05-29 00:13:16 +0800

    fix: fix issue of divide by zero

--- app/src/main/kotlin/edu/whu/gitool/utility/Utils.kt
+++ app/src/main/kotlin/edu/whu/gitool/utility/Utils.kt
@@ -2,6 +2,7 @@ package edu.whu.gitool.utility
 
 object Utils {
     fun toPercentage(cnt: Int, total: Int, efficient: Int = 2): String {
+        if (total == 0) return "NaN"
         val percentage = (cnt.toDouble() / total * 100).toString()
         return "%.${efficient}f%%".format(percentage.toDouble())
     }
